5afbbf33125556f89168c280a6dcd0f43986ce0e
Shouvanik21 <shouvanik210305@gmail.com>
2024-12-06 22:58:56 +0530

    updated commit

--- Profile-Card/src/components/ProfileCard.jsx
+++ Profile-Card/src/components/ProfileCard.jsx
@@ -3,9 +3,11 @@ import ProfilePic from "../assets/img.jpg";
 function ProfileCard(){
     return (
         <>
-            <img className="profile-pic" src={ProfilePic} alt="profile pic"></img>
-            <h2 className="profile-title">Bro</h2>
-            <p className="profile-description">This is Bro</p>
+            <div className="container">
+                <img className="profile-pic" src={ProfilePic} alt="profile pic"></img>
+                <h2 className="profile-title">Bro</h2>
+                <p className="profile-description">This is Bro</p>
+            </div>            
         </>
     );
 }
