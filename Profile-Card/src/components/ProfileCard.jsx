f662fec59f31611c2f21f353e1bad1711c82f207
Shouvanik21 <shouvanik210305@gmail.com>
2024-12-06 23:30:00 +0530

    updated commit

--- Profile-Card/src/components/ProfileCard.jsx
+++ Profile-Card/src/components/ProfileCard.jsx
@@ -5,8 +5,8 @@ function ProfileCard(){
         <>
             <div className="container">
                 <img className="profile-pic" src={ProfilePic} alt="profile pic"></img>
-                <h2 className="profile-title">Bro</h2>
-                <p className="profile-description">This is Bro who is an youtuber and loves to play video games and football</p>
+                <h2 className="profile-title">Atreon</h2>
+                <p className="profile-description">This is Atreon who is an youtuber and loves to play video games and football</p>
             </div>            
         </>
     );
