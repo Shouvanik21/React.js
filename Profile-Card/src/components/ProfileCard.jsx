c91446705e6068f998c4ec875cc360a83900af98
Shouvanik21 <shouvanik210305@gmail.com>
2024-12-06 22:55:02 +0530

    updated commit

--- Profile-Card/src/components/ProfileCard.jsx
+++ Profile-Card/src/components/ProfileCard.jsx
@@ -1,7 +1,11 @@
+import ProfilePic from "../assets/img.jpg";
+
 function ProfileCard(){
     return (
         <>
-            
+            <img src={ProfilePic} alt="profile pic"></img>
+            <h2>Bro</h2>
+            <p>This is Bro</p>
         </>
     );
 }
